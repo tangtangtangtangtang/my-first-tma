03bd2559ff24796c8ba8373113722977977bdfc3
jackson <tangxiaozhuo@kuaishou.com>
2024-07-04 15:24:12 +0800

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import webapp from '@twa-dev/sdk'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="card">
+        <button onClick={() => webapp.showAlert(`Hello World! Current count is ${count}`)}>
+            Show Alert
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
